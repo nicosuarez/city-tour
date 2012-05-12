69426f20664d10b34854aeb751337b750bc32f78
ralejandro22000 <ralejandro22000@1a47bddf-91cb-1dea-4482-701ded4ccaa9>
2012-05-12 23:58:33 +0000

    model and data access mappings

---
+++ trunk/CityTour/web.Test/ModelTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using web.Models;
+
+namespace SampleTesting
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class ModelTest
+    {
+        [TestMethod]
+        public void GetAllPersonsMustSucceed()
+        {
+            CityTourEntities entities = new CityTourEntities();
+            IEnumerable<Person> persons = entities.Person;
+
+            Assert.IsTrue(persons.Count() > 0);
+        }
+    }
+}
