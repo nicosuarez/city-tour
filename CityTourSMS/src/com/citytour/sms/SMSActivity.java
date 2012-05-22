fcb45e0102de8aabe9eef762fba581aa1ea93a91
nicolas.suarez <nicolas.suarez@1a47bddf-91cb-1dea-4482-701ded4ccaa9>
2012-05-22 01:22:18 +0000

    Agrego aplicacion de Android para simular el procesamiento de sms

---
+++ CityTourSMS/src/com/citytour/sms/SMSActivity.java
@@ -0,0 +1,13 @@
+package com.citytour.sms;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SMSActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }  
+}
