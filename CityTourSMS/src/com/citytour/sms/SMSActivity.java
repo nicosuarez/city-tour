1bdf8eed80772c9c3b37814b778507981635a54e
nicolas.suarez <nicolas.suarez@1a47bddf-91cb-1dea-4482-701ded4ccaa9>
2012-05-28 01:45:39 +0000

    Implementacion envio de MMS

--- CityTourSMS/src/com/citytour/sms/SMSActivity.java
+++ CityTourSMS/src/com/citytour/sms/SMSActivity.java
@@ -1,6 +1,8 @@
 package com.citytour.sms;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class SMSActivity extends Activity {
@@ -9,5 +11,16 @@ public class SMSActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }  
+        SmsReceiver.smsActivity = this;
+    }
+    
+    public void sendMMS(String phoneNumber, String message, String filePath, String mimeType)
+    {    	    	
+    	Intent smsIntent = new Intent(Intent.ACTION_SEND);           	
+    	smsIntent.putExtra("address", phoneNumber);
+    	smsIntent.putExtra("sms_body", message);
+    	smsIntent.setType(mimeType);
+    	smsIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(filePath));    	    	
+    	startActivity(smsIntent);    	
+    }
 }
