10e4cc796dc4e0b9283f563a78f88a5019c4a0d3
ralejandro22000 <ralejandro22000@1a47bddf-91cb-1dea-4482-701ded4ccaa9>
2012-05-30 00:29:35 +0000

    event locations serialized.

---
+++ CityTour/web/Views/DataContracts/Assemblers/LocationAssembler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using web.Models;
+
+namespace web.Views.DataContracts.Assemblers
+{
+    public class LocationAssembler
+    {
+        public static LocationData Assemble(Location location)
+        {
+            return new LocationData
+            {
+                ID = location.ID,
+                Latitude = location.Latitude,
+                Longitude = location.Longitud,
+                Name = location.Name
+            };
+        }
+
+        public static IEnumerable<LocationData> Assemble(IQueryable<Location> locations)
+        {
+            return locations.Select(l => LocationAssembler.Assemble(l));
+        }
+    }
+}
