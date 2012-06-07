15f84938f4ed6bf2b771717152e08758dd26c7bd
malcha <malcha@1a47bddf-91cb-1dea-4482-701ded4ccaa9>
2012-06-07 00:57:58 +0000

    search

---
+++ trunk/CityTour/web/Controllers/SearchByController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using web.Models;
+
+namespace web.Controllers
+{
+    public class SearchByController : Controller
+    {
+        //
+        // GET: /SearchBy/
+
+        public ActionResult Index()
+        {
+            
+            return View(new SearchBy());
+        }
+
+        public ActionResult Search(string comecommerceName, string commerceDescription, string commerceCategoryID)
+        {
+            return Index() ;
+        }
+
+    }
+}
