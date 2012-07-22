a8c66199e509d9963db115db474aacbee8b9a73f
ralejandro22000 <ralejandro22000@1a47bddf-91cb-1dea-4482-701ded4ccaa9>
2012-07-22 22:45:16 +0000

    clear markers and lines fixed.

--- trunk/CityTour/web/Scripts/site.js
+++ trunk/CityTour/web/Scripts/site.js
@@ -9,6 +9,18 @@
         if (currentMapContainer.length > 0) {
             $('#citytourMap').appendTo(currentMapContainer);
         }
+
+        switch(ui.index){ 
+            case 0: 
+                $(window).trigger('nearLocations');
+                break;
+                
+            case 2: 
+                $(window).trigger('itinerary');
+                break;                
+        }
+        
+
     });
     $('#homeTabs').bind('tabsshow', function (event, ui) {
         if (citytour && citytour.map) {
@@ -16,15 +28,16 @@
         }
     });
 
-    if (citytour && citytour.map) {
-        var places = [
-            { name: 'Restaurant X', lat: -0.002, long: 0.002, ranking: 4 },
-            { name: 'Hotel Y', lat: -0.002, long: -0.002, ranking: 3 },
-            { name: 'Teatro Z', lat: 0.002, long: 0.002, ranking: 2 },
-            { name: 'Restaurant A', lat: 0.002, long: -0.002, ranking: 1 }
-        ];
+    $(window).bind('nearLocations', function () {
+        if (citytour && citytour.map) {
+            var places = [
+                    { name: 'Restaurant X', lat: -0.002, long: 0.002, ranking: 4 },
+                    { name: 'Hotel Y', lat: -0.002, long: -0.002, ranking: 3 },
+                    { name: 'Teatro Z', lat: 0.002, long: 0.002, ranking: 2 },
+                    { name: 'Restaurant A', lat: 0.002, long: -0.002, ranking: 1 }
+                ];
 
-        citytour.map.setLocations(places);
-    }
-
+            citytour.map.setLocations(places);
+        }
+    })
 });
