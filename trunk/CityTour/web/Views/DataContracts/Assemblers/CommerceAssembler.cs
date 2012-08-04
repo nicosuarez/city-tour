c35e9952472b8d9c96295669767e1b956cfb326e
ralejandro22000 <ralejandro22000@1a47bddf-91cb-1dea-4482-701ded4ccaa9>
2012-08-04 00:41:31 +0000

    mejoras

---
+++ trunk/CityTour/web/Views/DataContracts/Assemblers/CommerceAssembler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using web.Models;
+
+namespace web.Views.DataContracts.Assemblers
+{
+    public class CommerceAssembler
+    {
+        public static CommerceData Assemble(Commerce commerce) 
+        {
+            return new CommerceData
+            {
+                Address = commerce.Address,
+                Name = commerce.Name,
+                Description = commerce.Description,
+                Location = LocationAssembler.Assemble(commerce.Location)
+            };
+        }
+    }
+}
---
+++ trunk/CityTour/web/Views/DataContracts/CommerceData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace web.Views.DataContracts
+{
+    [DataContract]
+    public class CommerceData
+    {
+        [DataMember(Name="name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "address")]
+        public string Address { get; set; }
+
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+
+        [DataMember(Name = "location")]
+        public LocationData Location { get; set; }
+    }
+}
