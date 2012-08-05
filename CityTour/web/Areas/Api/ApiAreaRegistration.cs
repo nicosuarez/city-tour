b3e5a4871bec3d777aaa9d52dfff0d70f2ba0992
leandromencias@gmail.com <leandromencias@gmail.com@1a47bddf-91cb-1dea-4482-701ded4ccaa9>
2012-08-05 20:56:19 +0000

    Llamada reserva de taxi.

---
+++ CityTour/web/Areas/Api/ApiAreaRegistration.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace web.Areas.Api
+{
+    public class ApiAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return @"Api";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                @"ApiDefault",
+                @"Api/{controller}/{action}"
+            );
+        }
+    }
+}
---
+++ CityTour/web/Areas/Api/Controllers/ReservationsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace web.Areas.Api.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        [HttpPost]
+        public JsonResult Taxi(string address)
+        {
+            string message = String.Concat(@"TAXI PREMIUM patente CFK 678, arriva en 10 minutos a ", address);
+            return Json(new { sms = message }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
