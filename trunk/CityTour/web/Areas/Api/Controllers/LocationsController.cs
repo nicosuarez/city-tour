85c1f6420505c354a5bc979bec6dd7d5986c2643
leandromencias@gmail.com <leandromencias@gmail.com@1a47bddf-91cb-1dea-4482-701ded4ccaa9>
2012-08-05 20:56:19 +0000

    Llamada reserva de taxi.

--- trunk/CityTour/web/Areas/Api/Controllers/LocationsController.cs
+++ trunk/CityTour/web/Areas/Api/Controllers/LocationsController.cs
@@ -2,17 +2,17 @@
 using System.Web.Mvc;
 using web.Models;
 
-namespace web.Areas.Mobile.Controllers
+namespace web.Areas.Api.Controllers
 {
-    public class JSONAPIController : Controller
+    public class LocationsController : Controller
     {
         [HttpGet]
-        public JsonResult GetNearLocations(decimal latitude, decimal longitude)
+        public JsonResult GetNear(decimal latitude, decimal longitude)
         {
             using (CityTourEntities entities = new CityTourEntities())
             {
                 var locationsQuery = from l in entities.Location
-                                     let offset = 0.004M
+                                     let offset = 0.01M // Aprox +-10 cuadras.
                                      let insideLatitude = (latitude - offset) <= l.Latitude && l.Latitude <= (latitude + offset)
                                      let insideLongitude = (longitude - offset) <= l.Longitud && l.Longitud <= (longitude + offset)
                                      where insideLatitude && insideLongitude
